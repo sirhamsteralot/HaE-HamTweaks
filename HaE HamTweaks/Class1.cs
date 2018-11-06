19949c9416d90ed8972b829262f648bada419b7b
sirhamsteralot <sirhamsteralot@gmail.com>
2018-11-06 21:54:51 +0100

    Project setup

--- HaE HamTweaks/Class1.cs
+++ HaE HamTweaks/Class1.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VRage.Plugins;
 
 namespace HaE_HamTweaks
 {
-    public class Class1
+    public class HaEHamTweaks : IPlugin
     {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(object gameInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
