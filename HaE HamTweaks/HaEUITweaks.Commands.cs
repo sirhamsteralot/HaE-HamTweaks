ff9629dbc6b4929e5944caf1d8439d1456059722
sirhamsteralot <sirhamsteralot@gmail.com>
2018-11-07 23:58:17 +0100

    Commands file

---
+++ HaE HamTweaks/HaEUITweaks.Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaE_HamTweaks
+{
+    public partial class HaEUITweaks
+    {
+        public void RegisterCommands()
+        {
+
+        }
+    }
+}
---
+++ HaE HamTweaks/HaEUXTweaks.Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaE_HamTweaks
+{
+    public partial class HaEUXTweaks
+    {
+
+        public void RegisterCommands()
+        {
+
+        }
+    }
+}
