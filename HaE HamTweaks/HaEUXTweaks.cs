1f4f4e969455f5166d12b1af601ea44b29b01cf1
SirHamsterAlot <SirHamsterAlot@gmail.com>
2018-11-19 13:16:26 +0100

    TODO: find a way to do this

--- HaE HamTweaks/HaEUXTweaks.cs
+++ HaE HamTweaks/HaEUXTweaks.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VRage;
+using VRage.Game;
+using VRage.Game.ObjectBuilders;
+using VRage.Game.ObjectBuilders.Definitions;
+using VRage.Game.ObjectBuilders.Definitions.SessionComponents;
+using Sandbox;
+using Sandbox.Game;
+using Sandbox.Game.SessionComponents;
+using Sandbox.Game.SessionComponents.Clipboard;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Cube;
 
 namespace HaE_HamTweaks
 {
     public partial class HaEUXTweaks
     {
+        private MyGridClipboard Clipboard => MyClipboardComponent.Static.Clipboard;
+
         public HaEUXTweaks()
         {
             OnInit();
@@ -27,5 +41,13 @@ namespace HaE_HamTweaks
         {
 
         }
+
+
+        #region methods
+        public void SetSnapModeOverride(SnapMode mode)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
