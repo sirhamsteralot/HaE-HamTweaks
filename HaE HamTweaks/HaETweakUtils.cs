b26326b3292f2a1450985661bc464e6dd2f3a541
sirhamsteralot <sirhamsteralot@gmail.com>
2020-01-12 13:47:45 +0100

    Easy way to get any value using the eval functionality

--- HaE HamTweaks/HaETweakUtils.cs
+++ HaE HamTweaks/HaETweakUtils.cs
@@ -1,6 +1,7 @@
 ﻿using Sandbox.Engine.Multiplayer;
 using Sandbox.Game.World;
 using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,63 @@ namespace HaEHamTweaks
                 return true;
 
             return false;
+        }
+
+        public static string GetAnyValueS(string SourceTypeName, string FieldName, object sourceObject = null)
+        {
+            try
+            {
+                Type type = null;
+                foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                {
+                    type = assembly.GetType(SourceTypeName, false);
+                    if (type != null)
+                        break;
+                }
+
+                if (type == null)
+                    return "Could not resolve type";
+
+                FieldInfo field = type.GetField(FieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+                PropertyInfo property = type.GetProperty(FieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+                if (field == null && property == null)
+                    return "Could not resolve field/property";
+
+                if (field != null)
+                    return field.GetValue(sourceObject)?.ToString() ?? "Null";
+                else
+                    return property.GetValue(sourceObject)?.ToString() ?? "Null";
+            }
+            catch (Exception)
+            {
+                return "Error";
+            }
+        }
+
+        public static object GetAnyValue(string SourceTypeName, string FieldName, object sourceObject = null)
+        {
+            Type type = null;
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                type = assembly.GetType(SourceTypeName, false);
+                if (type != null)
+                    break;
+            }
+
+            if (type == null)
+                return null;
+
+            FieldInfo field = type.GetField(FieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+            PropertyInfo property = type.GetProperty(FieldName, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
+
+            if (field == null && property == null)
+                return null;
+
+            if (field != null)
+                return field.GetValue(sourceObject)?.ToString();
+            else
+                return property.GetValue(sourceObject)?.ToString();
         }
     }
 }
