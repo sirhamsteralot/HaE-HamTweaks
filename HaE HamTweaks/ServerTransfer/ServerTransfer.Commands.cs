ebf218c406afed487ec0348f6806956b1dedfc24
sirhamsteralot <sirhamsteralot@gmail.com>
2020-08-12 00:54:10 +0200

    server transfer

---
+++ HaE HamTweaks/ServerTransfer/ServerTransfer.Commands.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaE_HamTweaks.ServerTransfer
+{
+    public partial class ServerTransfer
+    {
+        public void RegisterCommands()
+        {
+
+        }
+    }
+}
---
+++ HaE HamTweaks/ServerTransfer/ServerTransfer.cs
@@ -0,0 +1,22 @@
+﻿using Sandbox.Game.Gui;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaE_HamTweaks.ServerTransfer
+{
+    public partial class ServerTransfer
+    {
+        public void SeamlessConnect()
+        {
+            MyJoinGameHelper.JoinGame()
+        }
+
+        public void SeamlessDisconnect()
+        {
+
+        }
+    }
+}
